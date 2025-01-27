ce8d5835a429bb404b93317ea0f82e2ce3bd626a
Dipak Patil <dipakup.a04@gmail.com>
2025-01-28 00:28:48 +0530

    Feat : khatabook added

--- Khatabook_App/khatabook.js
+++ Khatabook_App/khatabook.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = 5000;
+const PORT = 3200;
 
 app.use(express.json());
 
