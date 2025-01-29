9244135ca633c9e3e35c260ee2361ffade6bd08e
Dipak Patil <dipakup.a04@gmail.com>
2025-01-29 23:55:49 +0530

    Feat : Change server port from 3200 to 3000

--- Khatabook_App/khatabook.js
+++ Khatabook_App/khatabook.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = 3200;
+const PORT = 3000;
 
 app.use(express.json());
 
